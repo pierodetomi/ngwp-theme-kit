a9e9544f18dbcd36980900b77d08f93320fbce12
Piero De Tomi <pierodetomi@gmail.com>
2022-02-13 01:58:28 +0100

    dotnet: Cleanup of functions.php template

--- dotnet/NgWP.ThemeBuilder/Templates/functions/functions.php
+++ dotnet/NgWP.ThemeBuilder/Templates/functions/functions.php
@@ -7,28 +7,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once("inc/helpers/theme-helper.php");
 
 {{theme-support-entries}}
-// ES:
-// add_theme_support("title-tag");
-// add_theme_support("post-thumbnails");
 
 add_action("customize_register", "configure_customizer");
-//add_action("after_setup_theme", "register_primary_menu");
-//add_action("admin_menu", "update_admin_menu");
 
 function configure_customizer($wp_customize) {
     {{sections-code}}
     
     {{settings-code}}
 }
-
-// if( function_exists("get_theme_file_path") ){
-// 	/** @noinspection PhpIncludeInspection */
-// 	require_once( get_theme_file_path( '/inc/oneartist-framework.php' ) );
-// }
-// else{
-// 	/** @noinspection PhpIncludeInspection */
-// 	require_once( get_template_directory() . '/inc/oneartist-framework.php' );
-// }
-
-// $oneartistFramework = new OneArtistFramework();
-// $oneartistFramework->construct();
